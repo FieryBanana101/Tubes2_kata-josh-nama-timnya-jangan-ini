7f0a4f75256b6f4139c097d4ba08c38a67791187
Cikang44 <jingglang@gmail.com>
2026-04-14 17:13:00 +0700

    feat: added tailwindcss

--- src/web.rs
+++ src/web.rs
@@ -12,7 +12,7 @@ fn App() -> Html {
     };
 
     html! {
-        <div>
+        <div class="m-20">
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
         </div>
