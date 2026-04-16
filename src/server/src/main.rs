2cd6d7465b4dc981fb909165d21513db15fac1de
Cikang44 <jingglang@gmail.com>
2026-04-16 10:25:03 +0700

    feat: added result attribute to query response

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -9,6 +9,7 @@ struct QueryPostBody {
     file_payload: String,
     url_payload: String,
     text_payload: String,   
+    use_dfs: bool,
 }
 
 #[derive(Serialize,Deserialize)]
@@ -16,13 +17,23 @@ struct QueryResponseNode {
     tag: String,
     class: String,
     id: String,
+    attributes: Vec<(String, String)>,
     children: Vec<u32>,
+}
+
+#[derive(Serialize,Deserialize)]
+struct ResultItem {
+    query: String,
+    paths: Vec<Vec<u32>>,
+    selected: Vec<u32>,
 }
 
 #[derive(Serialize,Deserialize)]
 struct QueryResponse {
     root_index: u32,
     nodes: Vec<QueryResponseNode>,
+    results: Vec<ResultItem>,
+    selected_nodes: Vec<u32>,
 }
 
 async fn query(body: web::Json<QueryPostBody>) -> HttpResponse {
@@ -33,33 +44,51 @@ async fn query(body: web::Json<QueryPostBody>) -> HttpResponse {
                 tag: "html".to_string(),
                 class: "".to_string(),
                 id: "".to_string(),
+                attributes: vec![],
                 children: vec![1],
             },
             QueryResponseNode {
                 tag: "body".to_string(),
                 class: "page-body".to_string(),
                 id: "".to_string(),
+                attributes: vec![],
                 children: vec![2, 3, 4],
             },
             QueryResponseNode {
                 tag: "div".to_string(),
                 class: "meow".to_string(),
                 id: "meow1".to_string(),
+                attributes: vec![],
                 children: vec![],
             },
             QueryResponseNode {
                 tag: "div".to_string(),
                 class: "meow meoww".to_string(),
                 id: "meow2".to_string(),
+                attributes: vec![],
                 children: vec![],
             },
             QueryResponseNode {
                 tag: "div".to_string(),
                 class: "meow meooow".to_string(),
                 id: "meow3".to_string(),
+                attributes: vec![],
                 children: vec![],
             },
         ],
+        results: vec![
+            ResultItem {
+                query: "body".to_string(),
+                paths: vec![vec![0, 1]],
+                selected: vec![1],
+            },
+            ResultItem {
+                query: ".meow".to_string(),
+                paths: vec![vec![1, 2], vec![1, 3], vec![1, 4]],
+                selected: vec![2, 3, 4],
+            },
+        ],
+        selected_nodes: vec![2, 3, 4],
     };
 
     HttpResponse::Ok().json(res)
